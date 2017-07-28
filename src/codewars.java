fff3cb4298e1ea23c8b28a5181d98703e3f90f05
Agnar92 <aganr1992@gmail.com>
2017-07-28 20:07:51 +0200

    fix for codewars

--- src/codewars.java
+++ src/codewars.java
@@ -1,9 +1,7 @@
-/**
- * Created by KomputerK on 29.06.2017.
- */
+
 import java.util.regex.*;
 public class codewars {
-    public static  howOld(final String herOld) {
+    public static int howOld(final String herOld) {
         Pattern p = Pattern.compile("[0-9]");
         Matcher m = p.matcher(herOld);
         while(m.find()){
